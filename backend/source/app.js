1e9ac792f07c7a8e8604028c6038516f6ccad23b
golitos <souzadasila101lucas@gmail.com>
2025-09-04 15:56:30 -0300

    feat(alunos, bancas): rotar GET /api/alunos e /api/bancas para preencher tabelas
    - GET /api/alunos: retorna id_trabalho, aluno, id_aluno, orientador, id_orientador, status, banca (ex: '0/2') -> esperando o frontend para refinar
    - GET /api/bancas: retorna id_trabalho, aluno, orientador, banca, nota -> também aguardando o frontend

--- backend/source/app.js
+++ backend/source/app.js
@@ -3,24 +3,27 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-//const alunoRoutes = require("./routes/alunoRoutes");
+const alunoRoutes = require("./routes/alunoRoutes");
 const cursoRoutes = require("./routes/cursoRoutes");
 const professorRoutes = require("./routes/professorRoutes");
 const trabalhoRoutes = require("./routes/trabalhoRoutes");
 const avaliacaoBancaRoutes = require("./routes/avaliacaoBancaRoutes");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const bancaRoutes = require("./routes/bancaRoutes");
 
 app.use(bodyParser.json());
 
 // passa o router importado
-//app.use("/alunos", alunoRoutes);
+app.use("/alunos", alunoRoutes);
 app.use("/cursos", cursoRoutes);
 app.use("/professores", professorRoutes);
 app.use("/trabalhos", trabalhoRoutes);
 app.use("/avaliacoes", avaliacaoBancaRoutes);
+
 app.use("/api", authRoutes);
-app.use("/api", userRoutes)
+app.use("/api", userRoutes);
+app.use("/api", bancaRoutes);
 
 app.get("/ping", (req, res) => res.json({ message: "pong 🏓" }));
 
